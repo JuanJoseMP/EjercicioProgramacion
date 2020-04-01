c8647f9b0ed843eebffb262500047a793aa2e541
Danraove <danraove@gmail.com>
2020-04-01 11:08:54 +0200

    programa principal Main

--- Ejercicio programacion grupo/Ejercicio programacion grupo/programa principal.cs
+++ Ejercicio programacion grupo/Ejercicio programacion grupo/programa principal.cs
@@ -6,6 +6,15 @@ namespace Ejercicio_programacion_grupo
 {
     class programa_principal
     {
+
+        public struct Datos
+        {
+            public string nombre;
+            public string autor;
+            public int paginas;
+            public int ayoPublicacion;
+
+        }
         static void Main(string[] args)
         {
 
