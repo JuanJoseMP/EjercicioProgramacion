5735c59ab89c8417d868ecba58e07bf9b94a081b
RodPlaza <rodrigo.plaza@campusfp.es>
2020-04-15 11:31:50 +0200

    Mostrar datos 1.0 y Tamaño datos 1.0

--- Ejercicio programacion grupo/Ejercicio programacion grupo/mostrar datos.cs
+++ Ejercicio programacion grupo/Ejercicio programacion grupo/mostrar datos.cs
@@ -6,6 +6,18 @@ namespace Ejercicio_programacion_grupo
 {
     class mostrar_datos
     {
-
+        public static void MostrarDatos(int contador ,ref datos[] datos)
+        {
+            for(int i = 0; i < contador; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Fichero {0}",contador+1);
+                Console.WriteLine("");
+                Console.WriteLine("Nombre: {0}", datos[i].nombre);
+                Console.WriteLine("Autor: {0}", datos[i].autor);
+                Console.WriteLine("Número de páginas: {0}", datos[i].paginas);
+                Console.WriteLine("Año de publicación: {0}", datos[i].ayoPublicacion);
+            }
+        }
     }
 }
--- Ejercicio programacion grupo/Ejercicio programacion grupo/tamaño datos.cs
+++ Ejercicio programacion grupo/Ejercicio programacion grupo/tamaño datos.cs
@@ -6,6 +6,23 @@ namespace Ejercicio_programacion_grupo
 {
     class tamaño_datos
     {
-
+        public static void tamañoDatos(int contador, ref datos[] datos)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Para buscar un dato por su tamaño, introduzca un número mínimo de páginas.");
+            int pagMin = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < contador; i++)
+            {
+                if (datos[i].paginas > pagMin)
+                {
+                    Console.WriteLine("Fichero {0}", i);
+                    Console.WriteLine("");
+                    Console.WriteLine("Nombre: {0}", datos[i].nombre);
+                    Console.WriteLine("Autor: {0}", datos[i].autor);
+                    Console.WriteLine("Número de páginas: {0}", datos[i].paginas);
+                    Console.WriteLine("Año de publicación: {0}", datos[i].ayoPublicacion);
+                }
+            }
+        }
     }
 }
